25c7fc32e564cc8ea2c3f87aced336edc9b8638f
Alex Dieulot <adieulot@gmail.com>
2016-05-20 11:40:57 +0200

    Fetch topic test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+  , http = require('http')
+  , router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/topic', function(req, res, next) {
+  let request = http.request({
+    hostname: 'www.jeuxvideo.com',
+    path: '/forums/42-1000021-47040669-1-0-1-0-les-voyages-de-presse-qu-en-est-il-posez-vos-questions.htm',
+  }, (res2) => {
+    let text = ''
+    res2.on('data', (chunk) => {
+      text += chunk
+    })
+    res2.on('end', () => {
+      res.contentType('text/plain')
+      res.send(text)
+    })
+  })
+
+  request.on('error', (e) => {
+    console.log(`error: ${e.message}`)
+    res.set('Content-Type', 'text/plain')
+    res.send('error')
+  })
+
+  request.end()
+})
 
 module.exports = router;
